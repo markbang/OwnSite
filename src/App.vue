aac288c54a92659bbb29ae766a107e93122533eb
markbang <3137972908@qq.com>
2024-03-10 15:02:32 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div className="flex items-center">
+    <n-button type="primary">naive-ui</n-button>
+    <h1 className="text-3xl font-bold underline text-red-600">
+        Hello world!
+    </h1>
+  </div>
 </template>
+
+<script setup lang="ts">
+  import { NButton } from 'naive-ui'
+</script>
 
 <style scoped>
 
