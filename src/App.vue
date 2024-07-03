bb8692acb8a8832a59a867464b9c5cfb8c8e4ae9
markbang <i@bangwu.top>
2024-07-03 18:42:43 +0800

    upup change framework and skills

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import AutoCome from './components/AutoCom.vue'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline bg-red-500 text-red-400">
+    Hello world!
+  </h1>
+  <AutoCome />
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AutoCom.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <div class="card flex justify-content-center">
+        <AutoComplete v-model="value" dropdown :suggestions="items" @complete="search" />
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const value = ref("");
+const items = ref([]);
+
+const search = (event) => {
+    let _items = [...Array(10).keys()];
+
+    items.value = event.query ? [...Array(10).keys()].map((item) => event.query + '-' + item) : _items;
+}
+</script>
